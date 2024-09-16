2a997b53ca9a4d0e7f87f3cc510b4a51aa0a891f
Matteo Vedovati <matteo.vedovati.77@gmail.com>
2024-09-16 15:30:46 +0200

    Update route.ts

--- src/web/my-app/app/api/products/route.ts
+++ src/web/my-app/app/api/products/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  console.log('-----------------API route hit');
+  console.error('-----------------API route hit - /api/products-----------------');
   const res = await fetch('http://app:3000/api/products');
   const products = await res.json();
   return NextResponse.json(products)
